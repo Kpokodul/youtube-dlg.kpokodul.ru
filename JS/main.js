a8e8db79075df5fb052ac18801d9addf7cd39940
Kpokodul <51588090+Kpokodul@users.noreply.github.com>
2021-01-20 21:25:29 +0300

    New site!

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+let downWin = "https://github.com/MrS0m30n3/youtube-dl-gui/releases/download/0.4/youtube-dl-gui-0.4-win-setup.zip" // downloadWin
+
+function downloadWin() {
+	window.open(downWin);
+}
+
+function download() {
+	location.href='#download';
+}
